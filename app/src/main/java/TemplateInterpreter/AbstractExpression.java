9ef9d283d9a0d5e8caae9651b6f076bf2f664d09
Khenya <abrendakhenya@gmail.com>
2023-02-22 21:48:33 -0400

    Template Interpreter

---
+++ app/src/main/java/TemplateInterpreter/AbstractExpression.java
@@ -0,0 +1,5 @@
+package TemplateInterpreter;
+
+public abstract class AbstractExpression {
+    public abstract void interpreter(Context context);
+}
---
+++ app/src/main/java/TemplateInterpreter/Context.java
@@ -0,0 +1,10 @@
+package TemplateInterpreter;
+
+public class Context {
+    protected String input="";
+    protected String output="";
+
+    public Context (String input) {
+        this.input = input;
+    }
+}
